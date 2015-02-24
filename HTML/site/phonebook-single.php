18925a28477c12c309f95827622b8344edc9f3c8
Shane Prendergast <contact@webknit.co.uk>
2015-02-24 22:01:13 +0000

    Adding sample admin options on the single phonebook page

--- HTML/site/phonebook-single.php
+++ HTML/site/phonebook-single.php
@@ -26,6 +26,13 @@
 
 					<h2 class="content__megaheader">Shane Prendergast</h2>
 
+					<ul class="admin-options">
+
+						<li class="admin-options__li"><a>Edit</a></li>
+						<li class="admin-options__li"><a>Delete</a></li>
+						
+					</ul>
+
 					<p><strong>Question</strong></p>
 					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 
